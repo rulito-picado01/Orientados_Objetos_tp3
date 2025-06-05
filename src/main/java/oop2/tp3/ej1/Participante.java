486ab4038b5d1c8a4bc5629f6bed44409d7f026e
Facundo <fgonzalezcarus@gmail.com>
2025-06-05 14:31:47 -0300

    cambios de refactorizacion ejercicio concurso

--- src/main/java/oop2/tp3/ej1/Participante.java
+++ src/main/java/oop2/tp3/ej1/Participante.java
@@ -1,16 +1,8 @@
 package oop2.tp3.ej1;
 
-import oop2.tp3.bd.ConexionBD;
-import oop2.tp3.mail.Mail;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Participante {
-    private String dni;
-    private String nombre;
+    private final String dni;
+    private final String nombre;
     private int puntos;
 
     public Participante(String dni, String nombre) {
@@ -23,15 +15,12 @@ public class Participante {
         this.puntos += puntos;
     }
 
-    public Inscripcion inscribirse(Concurso concurso) {
-        Inscripcion inscripcion = concurso.inscribirParticipante(this);
+    public Inscripcion inscribirseEn(Concurso concurso) {
+        return concurso.inscribirParticipante(this);
+    }
 
-        if (inscripcion != null) {
-            Mail emailService = new Mail();
-            emailService.enviarMail(this.getDni(), this.nombre, concurso.getNombre());
-        }
-
-        return inscripcion;
+    public void guardarEn(RepositorioDeParticipantes repositorio) {
+        repositorio.guardar(this);
     }
 
     public int puntosGanados() {
@@ -42,32 +31,20 @@ public class Participante {
         return dni;
     }
 
-    public void guardarEnBaseDeDatos() {
-        String sqlCheck = "SELECT dni FROM participantes WHERE dni = ?";
-        String sqlInsert = "INSERT INTO participantes (dni, nombre) VALUES (?, ?)";
-
-        try (Connection conn = ConexionBD.conectarBDC();
-             PreparedStatement checkStmt = conn.prepareStatement(sqlCheck)) {
-
-            checkStmt.setInt(1, Integer.parseInt(this.dni));
-            ResultSet rs = checkStmt.executeQuery();
-
-            if (rs.next()) {
-                System.out.println("Participante ya existe en la BD, no se vuelve a insertar.");
-                return; // Ya existe, evitamos duplicado
-            }
-
-            try (PreparedStatement insertStmt = conn.prepareStatement(sqlInsert)) {
-                insertStmt.setInt(1, Integer.parseInt(this.dni));
-                insertStmt.setString(2, this.nombre);
-                insertStmt.executeUpdate();
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error al guardar participante en BD: " + e.getMessage());
-            e.printStackTrace();
-        }
+    public String getNombre() {
+        return nombre;
     }
 
-}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Participante)) return false;
+        Participante that = (Participante) o;
+        return dni.equals(that.dni);
+    }
 
+    @Override
+    public int hashCode() {
+        return dni.hashCode();
+    }
+}
